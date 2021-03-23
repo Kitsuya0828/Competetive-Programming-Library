92dd6936910d1ef34037c7a6707f0e5e23bc206c
alvin <kitsuyaazuma@gmail.com>
2021-03-23 12:03:10 +0900

    make it better

---
+++ C++ Library/Binary_Search.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// x が条件を満たすかどうか
+bool P(int x) {
+
+}
+
+// P(x) = true となる最小の整数 x を返す
+int binary_search() {
+    int left, right;   // P(left)=false, P(right)=true となるように
+
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+        if (P(mid)) right = mid;
+        else left = mid;
+    } 
+    return right;
+}
