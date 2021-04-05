20aa579f82f30affe9bdf5fce2c9ac0f52fde259
alvin <kitsuyaazuma@gmail.com>
2021-04-06 00:12:19 +0900

    make it better

---
+++ C++_Library/Insertion_Sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 【挿入ソート】
+// 平均時間計算量 O(N^2)
+// 最悪時間計算量 O(N^2)   <-- 降順に並んでいるケース 0+1+...+N-1 = N(N-1)/2
+// 追加で必要な外部メモリ容量 O(1) <-- 変数 v
+// in-place & 安定なソート
+
+
+// 配列 a をソートする
+void InsertionSort(vector<int> &a) {
+    int N = (int)a.size();
+    for (int i=1; i<N; i++){
+        int v = a[i];  // 挿入したい値
+
+        // v を挿入する適切な場所 j を探す
+        int j = i;
+        for (; j>0; j--) {
+            if (a[j-1] > v) {   // v より大きいものは 1 つ後ろに移す
+                a[j] = a[j-1];
+            }
+            else break;   // v 以下になったら止める（右から見ていくから安定ソート）
+        }
+        a[j] = v;    // 最後に j 番目に v をもってくる
+    }
+}
+
+int main() {
+    // 入力
+    int N; // 要素数
+    cin >> N;
+    vector<int> a(N);
+    for (int i=0; i<N; i++) {
+        cin >> a[i];
+    }
+
+    // 挿入ソート
+    InsertionSort(a);
+}
